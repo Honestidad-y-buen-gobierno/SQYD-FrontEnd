e9697f17d2d34660c2e4e96b423cab9a9f68afa6
CRZAndroid <josuegaldamezc@gmail.com>
2025-01-07 12:15:08 -0600

    Add header implementation

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
-import NavBar from "./components/NavBar";
-//import Home from "./pages/Home";
-import Denuncia from "./pages/Denuncia";
-//import Seguimiento from "./pages/Seguimiento";
-import "./assets/styles/App.css"; // Archivo de estilos globales
 
 function App() {
   return (
-    <Router>
-      <div className="app-container">
-        <Header />
-        <NavBar />
-        <main className="main-content">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/denuncia" element={<Denuncia />} />
-            <Route path="/seguimiento" element={<Seguimiento />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="App">
+      <Header />
+      <main>
+        <h1>Bienvenido a la página principal</h1>
+        <p>opciones.</p>
+      </main>
+    </div>
   );
 }
 
